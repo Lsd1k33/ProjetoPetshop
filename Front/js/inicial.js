1ce6d61c89dab5c36b7bc114eec636a99b7d2db9
Jorge Luiz de S Duarte <lsd1k33@gmail.com>
2019-09-18 22:29:17 -0300

    Criando página de testes durante a aula, apresentando erro na index.html

---
+++ Front/js/inicial.js
@@ -0,0 +1,18 @@
+// TROCA O TITULO
+function myFunction() {
+  document.getElementById("demo").innerHTML = "THIS IS BRAZIL!";
+}
+
+function myFunction2() {
+  document.getElementById("demo").innerHTML = "THIS IS ARGENTINA!";
+}
+
+// TROCA O BACKGROUND
+function luz() {
+  document.getElementById("bodyID").classList = "backgroundluz";
+  document.getElementById("( ͡° ͜ʖ ͡°)").classList = "backgroundluz";
+}
+function escuro() {
+  document.getElementById("bodyID").classList = "backgroundescuro";
+  document.getElementById("( ͡° ͜ʖ ͡°)").classList = "backgroundescuro";
+}
